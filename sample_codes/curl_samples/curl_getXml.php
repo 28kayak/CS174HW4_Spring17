56754026313fdc2897129684cc8be124f356b7b1
28kayak <kaya.ota@sjsu.edu>
2017-03-28 15:32:46 -0700

    edit

--- sample_codes/curl_samples/curl_getXml.php
+++ sample_codes/curl_samples/curl_getXml.php
@@ -7,18 +7,19 @@
  * https://teratail.com/questions/29447
  */
 
-function call_lookup_stockAPI($url, $input)
+function call_lookup_stockAPI($url)
 {
     //initialize curl
     $curl = curl_init($url);
     //create an option array
     $options = array(
         //Header
-        CURLOPT_HEADER => array(
-            'accept: application/xml',
-            'Content-type: text/plain; charset=UTF-8'
+        CURLOPT_HEADER => false,
+            //array(
+            //'accept: application/xml',
+            //'Content-type: text/plain; charset=UTF-8'
 
-        ),
+        //),
         //method
         CURLOPT_HTTPGET => true,
         CURLINFO_CONTENT_TYPE => 'application/xml',
@@ -38,9 +39,21 @@ function call_lookup_stockAPI($url, $input)
     if($result == true)
     {
         echo "result is true<br>";
-        echo $result;
-        $test = htmlspecialchars($result);
-        $xml = new SimpleXMLElement($test);
+        echo $result;// at this point, it is string-like
+        echo "<br>";
+        //$test = htmlspecialchars($result);
+        //echo $test;
+        $xml = simplexml_load_string($result);
+       // $json = json_encode($xml);
+       // $data = json_decode($json);
+        echo "<br> show xml <br>";
+        $list = $xml->getName();
+        echo $xml->getName() ."<br>";
+        echo $xml->LookupResult->Symbol ."<br>";
+        $xml->asXML("test.xml");
+
+        return $xml;
+
     }
     else{
         echo "result is false";
@@ -50,18 +63,19 @@ function call_lookup_stockAPI($url, $input)
 
     //$result = simplexml_load_string($result);
     //echo $result ."<br>";
+    echo $xml;
     curl_close($curl);
     return $xml;
 }//call_lookup_stockAPI
 
 
-echo "hello world<br>";
+//echo "hello world<br>";
 $uri = "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/xml?input=APPL";
-$data = call_lookup_stockAPI($uri, "");
+$data = call_lookup_stockAPI($uri);
 echo "<br>data <br>";
-echo "<pre>";
+
 echo $data;
-echo "</pre>";
+
 //echo "<pre>".$data."</pre>";
 //$xml = new SimpleXMLElement($data);
 //echo $xml;
