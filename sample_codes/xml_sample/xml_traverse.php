5ba97ba742c38d765d6de334b987f261035ebadc
28kayak <kaya.ota@sjsu.edu>
2017-04-04 20:22:31 -0700

    + xml sample code

---
+++ sample_codes/xml_sample/xml_traverse.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: kaya
+ * Date: 4/4/2017
+ * Time: 8:04 PM
+ */
+$xml=simplexml_load_file("books.xml") or die("Error: Cannot create object");
+echo $xml->book[0]->title . "<br>";
+echo $xml->book[1]->title;
+
+?>
+
